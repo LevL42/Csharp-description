7494b55944738eec681ac948cc299dc21c808e80
Lev Likhachev <lev.i.lena@yandex.ru>
2022-11-06 19:07:57 +0700

    Добавил несколько методов и описание к ним

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            // - Используется для коментариев.
+            /* - Используется для больших
+             коментариев на 
+             много строк */
+            // Console.WriteLine("Hello"); Данный метод выводит информацию каждый раз в новой строке.
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            // Console.Write("Hello");  Данный метод выводит информацию в одну строчку. 
+            Console.Write("Hello");
+            Console.Write("Hello");
+            //Console.Write("Hello \n"); Вданном случае \n образует пропуск строки.
+            Console.Write("Hello \n");
+            Console.Write("Hello \n");
+            Console.Write("Hello");
+            // Console.ReadLine(); Данный метод позволяет в водить множество символов и завершается нажатием клавиши ENTER.
+            Console.ReadLine();
+            // Console.ReadKey(); Данный метод ожидает от пользователя нажатия на любую клавишу и завершает работу
+            Console.ReadKey();
+
+        }
+    }
+}
